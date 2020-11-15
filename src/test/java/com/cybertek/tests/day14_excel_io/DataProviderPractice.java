ab65f0e6b9c950d465fbb6a3dbefaeb0c5ba387f
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-11-15 14:48:08 -0500

    added excel util class

--- src/test/java/com/cybertek/tests/day14_excel_io/DataProviderPractice.java
+++ src/test/java/com/cybertek/tests/day14_excel_io/DataProviderPractice.java
@@ -1,10 +1,15 @@
 package com.cybertek.tests.day14_excel_io;
 
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class DataProviderPractice {
 
@@ -20,7 +25,21 @@ public class DataProviderPractice {
 
     @Test
     public void ddtLoginTest1(){
+        //login
+        usernameElement.sendKeys("storemanager85");
+        passwordElement.sendKeys("UserUser123", Keys.ENTER);
+        System.out.println(Driver.getDriver().getTitle());
+    }
+
+    @BeforeMethod
+    public void setup(){
         Driver.getDriver().get("https://qa1.vytrack.com/user/login");
         Driver.getDriver().manage().window().maximize();
+        Driver.getDriver().manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        Driver.closeDriver();
     }
 }
